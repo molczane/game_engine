92214c5029872805857163662bef1db56cdaa61d
Ernest Mołczan <128298715+molczane@users.noreply.github.com>
2023-10-29 20:54:34 +0100

    Add files via upload

---
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Hazel/Window.h"
+
+//my idea
+#include "Hazel/Log.h"
+
+#include "GLFW/glfw3.h"
+
+namespace Hazel
+{
+	class WindowsWindow : public Window
+	{
+	public:
+		WindowsWindow(const WindowProps& props);
+		virtual ~WindowsWindow();
+
+		void OnUpdate() override;
+
+		inline unsigned int GetWidth() const override { return m_Data.Width; }
+		inline unsigned int GetHeight() const override { return m_Data.Height;  }
+
+		//Window atributes 
+		inline void SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
+		void SetVSync(bool enabled) override;
+		bool IsVSync() const override;
+
+		inline virtual void* GetNativeWindow() const { return m_Window; }
+
+	private:
+		virtual void Init(const WindowProps& props);
+		virtual void Shutdown();
+	private:
+		GLFWwindow* m_Window;
+
+		struct WindowData
+		{
+			std::string Title;
+			unsigned int Width, Height;
+			bool VSync;
+
+			EventCallbackFn EventCallback;
+		};
+
+		WindowData m_Data;
+	};
+}
+
+
+
