aa0d9f6f1ffe2eb36b71c774cfc0fd4d00908cc3
Ernest Mołczan <128298715+molczane@users.noreply.github.com>
2023-10-29 20:47:31 +0100

    Add files via upload

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// For use bu Hazel applications
+
+#include "Hazel/Application.h"
+#include "Hazel/Layer.h"
+#include "Hazel/Log.h"
+#include "Hazel/ImGui/ImGuiLayer.h"
+
+//------------ ENTRY POINT ---------------//
+#include "Hazel/EntryPoint.h"
+//-----------------------------------------//
+
---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+//data structures
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "Hazel/Log.h" // figured it out well
+
+#ifdef HZ_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif // HZ_PLATFORM_WINDOWS
+
